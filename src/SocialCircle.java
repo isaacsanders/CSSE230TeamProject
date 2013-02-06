041faf391086ae18d63bdb6d8049daeb0f6e54be
campbeeg <campbeeg@rose-hulman.edu>
2013-02-06 16:07:18 -0500

    Updated SocialCircle and Group class

--- src/SocialCircle.java
+++ src/SocialCircle.java
@@ -1,9 +1,27 @@
+import java.util.HashMap;
+
 /**
- * TODO Put here a description of what this class does.
- *
- * @author campbeeg.
- *         Created Feb 3, 2013.
+ * SocialCircle is a class that holds and manipulates all Users, Groups, and
+ * user input
+ * 
+ * @author campbeeg. Created Feb 3, 2013.
  */
 public class SocialCircle {
+	private HashMap<String, User> members;
 
+	/**
+	 * Constructs a new SocialCircle with an empty members HashMap
+	 * 
+	 */
+	public SocialCircle() {
+		this.members = new HashMap<String, User>();
+	}
+
+	private void add(User newMember) {
+		this.members.put(newMember.getID(), newMember);
+	}
+
+	private void delete(User deleted) {
+		this.members.remove(deleted.getID());
+	}
 }
