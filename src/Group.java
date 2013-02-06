e535c55a27f2120fdcc119c2ebc312f4a27a484f
campbeeg <campbeeg@rose-hulman.edu>
2013-02-06 15:45:08 -0500

    Updated group class

--- src/Group.java
+++ src/Group.java
@@ -2,31 +2,39 @@ import java.util.ArrayList;
 
 /**
  * The Group class contains an ArrayList that stores all of its members.
- *
- * @author campbeeg.
- *         Created Feb 6, 2013.
+ * 
+ * @author campbeeg. Created Feb 6, 2013.
  */
 public class Group {
+
+	private String name;
 	private ArrayList<User> members;
-	
+
 	/**
-	 * TODO Put here a description of what this constructor does.
-	 *
+	 * Constructs an empty group with the specified name
+	 * 
+	 * @param name
+	 * 
 	 */
-	public Group() {
+	public Group(String name) {
 		this.members = new ArrayList<User>();
+		this.name = name;
 	}
-	
+
 	/**
-	 * TODO Put here a description of what this constructor does.
-	 *
+	 * Constructs a group with the members in the arrayList and with the
+	 * specified name
+	 * 
+	 * @param name
+	 * 
 	 * @param members
 	 */
-	public Group(ArrayList<User> members) {
+	public Group(String name, ArrayList<User> members) {
+		this.name = name;
 		this.members = members;
 	}
-	
+
 	private boolean add(User newMember) {
-		
+		return this.members.add(newMember);
 	}
 }
