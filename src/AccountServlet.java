56133a35cda2dc3540daf90296e29baa9d8a16e6
John McCormack <mccormjt@rose-hulman.edu>
2013-02-14 04:28:58 -0500

    updates

--- src/AccountServlet.java
+++ src/AccountServlet.java
@@ -11,10 +11,41 @@ import javax.servlet.http.*;
  */
 public class AccountServlet extends HttpServlet {
 
-	private final String EMPTY = "";
+	private static final String EMPTY = "";
+	private static final String CHANGENAME = "changeName";
+	private static final String ADDFRIEND = "addFriend";
 
 	/**
-	 * takes in the user inputs from a "sign up"
+	 * takes in the user inputs for changing user information
+	 * 
+	 * @param request
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// check session
+		HttpSession session = request.getSession(false);
+		
+		//get necessary parameters
+		String type = request.getParameter(CHANGENAME);
+		if (type == CHANGENAME){
+			//get exact paramters
+			String first = request.getParameter("firstName");
+			String last = request.getParameter("lastName");
+			//get and update user
+			 User user = User.find(session.getValue("ID").toString());
+			 user.setName(first + " " + last);
+			 user.save();
+		}
+		
+		//end with redirect to account page
+		doGet(request, response);
+	}
+	
+	/**
+	 * takes in the user inputs for changing user information
 	 * 
 	 * @param request
 	 * @throws IOException
@@ -27,6 +58,14 @@ public class AccountServlet extends HttpServlet {
 
 		// check session
 		HttpSession session = request.getSession(false);
+		
+		//get user
+		User user = User.find(session.getValue("ID").toString());
+		String fullName = user.getName();
+		int indexSplitter = fullName.indexOf(" ");
+		String first = fullName.substring(0, indexSplitter);
+		String last =  fullName.substring(indexSplitter + 1, fullName.length());
+		
 
 		// check if user is already logged in, if so go to main search page, if
 		// not allow sign up
@@ -52,7 +91,26 @@ public class AccountServlet extends HttpServlet {
 					+ "<a href=\"AccountServlet\">Account</a>"
 					+ "<a href=\"LogoutServlet\"><button class=\"logoutButton\" type=\"submit\">Log Out</button></a>"
 					+ "<a href=\"/Home\"><img class=\"socialCircleTitle\" src=\"socialCircle.png\" width=\"\" height=\"\" alt=\"Social Circle\"/></a>"
-					+ "</div>" + "<div id=\"content\">" + "</div>" + "</body>"
+					+ "</div>" 
+					+ "<div id=\"content\">"
+					+ "<fieldset><legend>Change Your Name</legend>"
+					+ "<form id=\"accountFormChangeName\" method=\"post\" action=\"AccountServlet\">"
+					+ "<label for=\"changeFirstName\">Change First Name</label>"
+					+ "<input name=\"changeFirstName\" id=\"changeFirstName\" type=\"text\" value=\""
+					//add users first name
+					+ first
+					+ "\" />"
+					+ "<label for=\"changeLastName\">Change Last Name</label>"
+					+ "<input name=\"changeLastName\" id=\"changeLastName\" type=\"text\" value=\" " 
+					//add users last name
+					+ last
+					+ "\"/>"
+					+ "</legend>"
+					+ "<input type=\"hidden\" name=\"type\" value=\"changeName\" />"
+					+ "<input class=\"button\" value=\"Change Name\" type=\"submit\" />"
+					+ "</form>"
+					+ "</div>" 
+					+ "</body>"
 					+ "</html>");
 
 		} else {
